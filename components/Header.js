5fe1f2b3fd8978afbe68a34e1790f057a1bd424d
Mark Brocato <mark.brocato@moovweb.com>
2020-03-19 01:30:11 +0200

    upgrade xdn

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {
   AppBar,
@@ -8,6 +8,7 @@ import {
   useScrollTrigger,
   Slide,
   Hidden,
+  LinearProgress,
 } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import Link from 'next/link'
@@ -15,12 +16,18 @@ import VersionChooser from './modules/VersionChooser'
 import SearchField from './SearchField'
 import GitHubButton from './GitHubButton'
 import Logo from './Logo'
-
+import Router from 'next/router'
 const useStyles = makeStyles(theme => ({
   appBar: {
     backgroundColor: theme.palette.background.default,
     boxShadow: 'none',
     borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  progress: {
+    bottom: 0,
+    height: 2,
+    left:0,
+    right: 0
   },
   root: {
     flexGrow: 1,
@@ -43,6 +50,12 @@ const useStyles = makeStyles(theme => ({
 export default function Header() {
   const trigger = useScrollTrigger()
   const classes = useStyles()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => setLoading(true))
+    Router.events.on('routeChangeComplete', () => setLoading(false))
+  }, [])
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
@@ -75,6 +88,7 @@ export default function Header() {
             <GitHubButton />
           </Toolbar>
         </Container>
+        <LinearProgress className={classes.progress} style={{ display: loading ? 'block' : 'none' }}/>
       </AppBar>
     </Slide>
   )

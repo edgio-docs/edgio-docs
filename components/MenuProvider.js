21796d3764806d870dd64598badd64d938656215
Mark Brocato <mark.brocato@moovweb.com>
2020-04-11 02:27:06 +0300

    close menu on page change

--- components/MenuProvider.js
+++ components/MenuProvider.js
@@ -1,9 +1,17 @@
-import React, { createContext, useState, useMemo } from 'react'
+import React, { createContext, useState, useMemo, useEffect } from 'react'
+import Router from 'next/router'
 
 export const MenuContext = createContext()
 
 export default function MenuProvider({ children }) {
   const [open, setOpen] = useState(false)
   const context = useMemo(() => ({ open, setOpen }), [open])
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => {
+      setOpen(false)
+    })
+  }, [])
+
   return <MenuContext.Provider value={context}>{children}</MenuContext.Provider>
 }

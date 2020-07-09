448950783c8af420617027bec55e7de5701945fc
Ishan Anand <ishan@moovweb.com>
2020-07-09 16:19:30 -0700

    Adding Google Tag Manager (GTM)

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,17 @@ class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
-        <Head>
+        <Head>          
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            const gtmID = window.location.hostname == "developer.moovweb.com" ? "GTM-5XH2P83" : "GTM-PQWGJWT";
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer',gtmID);`,
+          }}>
+          </script>
           <meta charSet="utf-8" />
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />

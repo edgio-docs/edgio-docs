bb1b0163ee39eaf7f3cea081974db6b18ce5a522
Mark Brocato <mark.brocato@moovweb.com>
2020-07-17 14:23:01 +0300

    Add withXDN next plugin.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
-const { withServiceWorker } = require('@xdn/next/sw')
+const { withXDN, withServiceWorker } = require('@xdn/next/config')
 
-module.exports = withServiceWorker({
-  target: 'serverless',
-  webpack: function(config) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    })
-    config.module.rules.push({
-      test: /\.(jpg|png)$/,
-      loader: 'url-loader',
-    })
-    return config
-  },
-})
+module.exports = withXDN(
+  withServiceWorker({
+    webpack: function (config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+      config.module.rules.push({
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+      })
+      return config
+    },
+  }),
+)

12afbbdd95399c2111443a9862ec5da5e663c356
Tristan Lee <tristanlee85@gmail.com>
2021-01-20 16:18:50 -0500

    Update to private keys

--- components/SearchField.js
+++ components/SearchField.js
@@ -1,11 +1,11 @@
 import { makeStyles } from '@material-ui/styles'
-import React, { useRef, useState, useCallback, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { ClickAwayListener, InputAdornment, TextField } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
-import SearchResults from './SearchResults'
 import useVersioning from './versioning'
-import lunr from 'lunr'
-import debounce from 'lodash/debounce'
+import searchConfig from '../docs.config.json'
+
+const { app_id: algoliaAppId, search_api_key: algoliaApiKey, index_name: indexName } = searchConfig
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,24 +24,28 @@ export default function SearchField() {
   const classes = useStyles()
   const { currentVersion } = useVersioning()
 
-  useEffect(() => {
-    const doEffect = async () => {
-      if (window.docsearch) {
-        docsearch({
-          apiKey: 'ac2f5be6d523eaf51cbf0ec8c629b882',
-          indexName: 'docs',
-          appId: '86GYGZYT5L',
-          inputSelector: '#docsearch',
-          algoliaOptions: {
-            hitsPerPage: 10,
-            // See https://www.algolia.com/doc/api-reference/api-parameters/
-          },
-          debug: true,
-        })
-      }
+  const onSearchLoad = () => {
+    if (window.docsearch) {
+      docsearch({
+        appId: algoliaAppId,
+        apiKey: algoliaApiKey,
+        indexName,
+        inputSelector: '#docsearch',
+        algoliaOptions: {
+          // See https://www.algolia.com/doc/api-reference/api-parameters/
+        },
+      })
     }
-    doEffect()
-  }, [currentVersion])
+  }
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://cdn.jsdelivr.net/npm/docsearch.js@latest/dist/cdn/docsearch.min.js'
+    script.async = true
+    script.onload = onSearchLoad
+
+    document.body.appendChild(script)
+  }, [])
 
   return (
     <div style={{ minWidth: 300 }}>
@@ -49,7 +53,6 @@ export default function SearchField() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/docsearch.js@latest/dist/cdn/docsearch.min.css"
       />
-      <script src="https://cdn.jsdelivr.net/npm/docsearch.js@latest/dist/cdn/docsearch.min.js"></script>
       <TextField
         placeholder="Search"
         variant="outlined"

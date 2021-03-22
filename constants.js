be18ce05660c951e419f274c154e918a2819abeb
Tristan Lee <tristanlee85@gmail.com>
2021-03-22 10:27:31 -0400

    Config for docs pages repo

--- constants.js
+++ constants.js
@@ -8,9 +8,10 @@ export const HEADER_PREFIX = 'x-layer0-'
 export const DOMAIN = 'layer0.co'
 export const APP_DOMAIN = `app.${DOMAIN}`
 export const DOCS_DOMAIN = `developer.${DOMAIN}`
+export const DOCS_PAGES_DOMAIN = 'layer0-docs.github.io'
 
 export const APP_URL = `https://${APP_DOMAIN}`
 export const FORUM_URL = `https://forum.${DOMAIN}`
 export const STATUS_URL = `https://status.${DOMAIN}`
 export const HELP_URL = `https://help.${DOMAIN}`
-export const DOCS_PAGES_REPO_URL = 'https://moovweb-docs.github.io/xdn-docs-pages'
+export const DOCS_PAGES_REPO_URL = `https://${DOCS_PAGES_DOMAIN}/layer0-docs-pages`
--- xdn.config.js
+++ xdn.config.js
@@ -1,13 +1,14 @@
 'use strict'
+const { DOCS_PAGES_DOMAIN } = require('./constants')
 
 // This file was automatically added by xdn deploy.
 // You should commit this file to source control.
 module.exports = {
   backends: {
     api: {
-      domainOrIp: 'moovweb-docs.github.io',
-      hostHeader: 'moovweb-docs.github.io',
+      domainOrIp: DOCS_PAGES_DOMAIN,
+      hostHeader: DOCS_PAGES_DOMAIN,
     },
   },
-  connector: '@xdn/next'
+  connector: '@xdn/next',
 }

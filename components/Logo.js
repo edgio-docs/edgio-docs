0624158c26bfc4e47f0498ebcd0a820860f3e696
Mark Brocato <mark.brocato@moovweb.com>
2021-04-13 09:58:03 +0300

    Fix logo.

--- components/Logo.js
+++ components/Logo.js
@@ -5,8 +5,8 @@ import Layer0 from './icons/logo-dark-full.svg'
 const useStyles = makeStyles(theme => ({
   logo: {
     height: 48,
-    marginBottom: -4,
-    width: 220,
+    marginBottom: -9,
+    marginLeft: -7,
     backgroundSize: 250,
     [theme.breakpoints.down('xs')]: {
       width: 150,

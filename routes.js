d83611af244ffff7d6cc0c3857b978b0b28cc5bc
Mark Brocato <mark.brocato@moovweb.com>
2021-04-28 18:28:45 +0300

    more CSP

--- routes.js
+++ routes.js
@@ -51,7 +51,7 @@ module.exports = new Router()
           `default-src 'self'`,
           `style-src 'unsafe-inline' 'self' fonts.googleapis.com cdn.jsdelivr.net`,
           `font-src fonts.gstatic.com`,
-          `img-src 'self' www.google-analytics.com analytics.twitter.com www.google.* www.facebook.com px.ads.linkedin.com tr.lfeeder.com data:`,
+          `img-src 'self' www.google-analytics.com analytics.twitter.com www.facebook.com px.ads.linkedin.com tr.lfeeder.com data:`,
           `frame-src www.youtube.com`,
           `script-src 'unsafe-inline' 'self' 'unsafe-eval' cdn.jsdelivr.net www.googletagmanager.com cdn.segment.com cdn4.mxpnl.com www.google-analytics.com widget.intercom.io sc.lfeeder.com snap.licdn.com connect.facebook.net www.youtube.com js.intercomcdn.com`,
           `base-uri 'self'`,

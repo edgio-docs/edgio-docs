f862343ae8b6c1ed660e172fce25328a4e099d5a
Tristan Lee <tristanlee85@gmail.com>
2021-05-21 13:35:06 -0400

    Add deploy buttons to home tiles

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,22 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav/Nav'
 import PageWrapper from '../components/PageWrapper'
-import { Typography, makeStyles, Container, Grid, Paper, Divider } from '@material-ui/core'
+import {
+  Typography,
+  makeStyles,
+  darken,
+  Container,
+  Grid,
+  Paper,
+  Divider,
+  Button,
+} from '@material-ui/core'
 import getBaseUrl from '../components/utils/getBaseUrl'
 import Link from 'next/link'
 import Layer0Icon from '../components/icons/layer0-black.svg'
 import Icon from '../components/icons/Icon'
 import Markdown from '../components/Markdown'
-import { DOCS_PAGES_REPO_URL, PRODUCT_NAME } from '../constants'
+import { DOCS_PAGES_REPO_URL, PRODUCT_NAME, EXAMPLES_REPOS } from '../constants'
 
 const useStyles = makeStyles(theme => ({
   hero: {
@@ -52,10 +61,14 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     alignItems: 'center',
     padding: theme.spacing(2),
-    cursor: 'pointer',
     '&:hover': {
       boxShadow: theme.shadows[8],
     },
+  },
+
+  frameworkWrapper: {
+    display: 'contents',
+    cursor: 'pointer',
   },
 
   frameworkText: {
@@ -84,6 +97,22 @@ const useStyles = makeStyles(theme => ({
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
   },
+  button: {
+    transition: 'color border-color 200ms linear',
+    '&:hover': {
+      borderColor: darken(theme.palette.secondary.light, 0.1),
+    },
+    '& span': {
+      textDecoration: 'none',
+    },
+    marginTop: 10,
+  },
+  buttonLink: {
+    textDecoration: 'none',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
 }))
 
 const iconProps = {
@@ -106,154 +135,95 @@ const Home = ({ navData, changeLog }) => {
       <Grid container className={classes.frameworks}>
         <Grid item xs={12}>
           <Grid container justify="center" spacing={2}>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/next">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="nextjs" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Next.js
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/next_commerce">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="next-commerce" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Start with Next.js Commerce
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/nuxt">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="nuxt" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Nuxt.js
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/vsf">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="vsf" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Vue Storefront
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/react">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="react" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with React</Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/vue">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="vue" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with Vue.js</Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/angular">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="angular" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Angular
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/react-storefront">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="react-storefront" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with React Storefront
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/sapper">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="sapper" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with Sapper</Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/gatsby">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="gatsby" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with Gatsby</Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/spartacus">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="spartacus" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Spartacus
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/nx">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="nx" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with Nx</Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/frontity">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="frontity" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Frontity
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/static_sites">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="html" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Static HTML/JS
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/ember_fastboot">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="fastboot" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>
-                    Get started with Ember Fastboot
-                  </Typography>
-                </Paper>
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/guides/[...guide]" as="/guides/razzle">
-                <Paper className={classes.framework} elevation={0}>
-                  <Icon type="razzle" className={classes.icon} {...iconProps} />
-                  <Typography className={classes.frameworkText}>Get started with Razzle</Typography>
-                </Paper>
-              </Link>
-            </Grid>
+            <FrameworkItem
+              guide="/guides/next"
+              framework="next"
+              icon="nextjs"
+              text="Get started with Next.js"
+            />
+
+            <FrameworkItem
+              guide="/guides/next_commerce"
+              framework="nextcommerce"
+              icon="next-commerce"
+              text="Start with Next.js Commerce"
+            />
+
+            <FrameworkItem guide="/guides/nuxt" framework="nuxt" text="Get started with Nuxt.js" />
+
+            <FrameworkItem
+              guide="/guides/vsf"
+              framework="vsf"
+              text="Get started with Vue Storefront"
+            />
+
+            <FrameworkItem
+              guide="/guides/react"
+              framework="static-react"
+              icon="react"
+              text="Get started with React"
+            />
+
+            <FrameworkItem
+              guide="/guides/vue"
+              framework="static-vue"
+              icon="vue"
+              text="Get started with Vue.js"
+            />
+
+            <FrameworkItem
+              guide="/guides/angular"
+              framework="angular"
+              text="Get started with Angular"
+            />
+
+            <FrameworkItem
+              guide="/guides/react-storefront"
+              framework="react-storefront"
+              text="Get started with React Storefront"
+            />
+
+            <FrameworkItem
+              guide="/guides/sapper"
+              framework="sapper"
+              text="Get started with Sapper"
+            />
+
+            <FrameworkItem
+              guide="/guides/gatsby"
+              framework="gatsby"
+              text="Get started with Gatsby"
+            />
+
+            <FrameworkItem
+              guide="/guides/spartacus"
+              framework="spartacus"
+              text="Get started with Spartacus"
+            />
+
+            <FrameworkItem guide="/guides/nx" framework="nx" text="Get started with Nx" />
+
+            <FrameworkItem guide="/guides/" framework="frontity" text="Get started with Frontity" />
+
+            <FrameworkItem
+              guide="/guides/static_sites"
+              framework=""
+              icon="html"
+              text=" Get started with Static HTML/JS"
+            />
+
+            <FrameworkItem
+              guide="/guides/ember_fastboot"
+              framework="fastboot"
+              text="Get started with Ember Fastboot"
+            />
+
+            <FrameworkItem
+              guide="/guides/razzle"
+              framework="razzle"
+              text="Get started with Razzle"
+            />
+
             <Grid item container xs={12} m={12} l={12} xl={12} justify="center">
               Don't see your framework?&nbsp;
               <Link href="/guides/[...guide]" as="/guides/traditional_sites">
@@ -326,4 +296,45 @@ Home.getInitialProps = async ({ version, versions, req }) => {
   ])
 
   return { navData, changeLog }
+}
+
+const FrameworkItem = ({ framework, text, guide, icon }) => {
+  const classes = useStyles()
+
+  if (!icon) icon = framework
+  return (
+    <Grid item>
+      <Paper className={classes.framework} elevation={0}>
+        <Link href="/guides/[...guide]" as={guide}>
+          <div className={classes.frameworkWrapper}>
+            <Icon type={icon} className={classes.icon} {...iconProps} />
+            <Typography className={classes.frameworkText}>{text}</Typography>
+          </div>
+        </Link>
+        <DeployLink framework={framework} />
+      </Paper>
+    </Grid>
+  )
+}
+
+const DeployLink = ({ framework }) => {
+  const classes = useStyles()
+  const url = EXAMPLES_REPOS[framework]
+
+  if (!url) {
+    console.warn(`Invalid deploy url for ${framework}`)
+    return null
+  }
+
+  return (
+    <a
+      className={classes.buttonLink}
+      href={`https://app.layer0.co/deploy?repo=${encodeURIComponent(EXAMPLES_REPOS[framework])}`}
+      target="_blank"
+    >
+      <Button variant="outlined" color="secondary" className={classes.button}>
+        Deploy to Layer0
+      </Button>
+    </a>
+  )
 }

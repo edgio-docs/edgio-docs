53d24c277e42de8a39e6c31d899a607751b4b1bb
Tristan Lee <tristanlee85@gmail.com>
2021-05-25 10:38:14 -0400

    Undo baseurl change (fn not used)

--- components/utils/getBaseUrl.js
+++ components/utils/getBaseUrl.js
@@ -3,7 +3,6 @@ function isLocalhost(host) {
 }
 
 export default function getBaseUrl(req) {
-  return 'http://127.0.0.1:3000'
   const host = req ? req.headers.host : ''
   return req ? `${isLocalhost(host) ? 'http' : 'https'}://${host}` : ''
 }

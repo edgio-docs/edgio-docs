f17149eedb74525265df4f97d9ceadd010dfbae0
Jeff Patzer <jeff.patzer@layer0.co>
2021-10-15 13:54:51 -0600

    add an edit on github link

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import { Divider, Grid, makeStyles, Button } from '@material-ui/core'
 import NextIcon from '@material-ui/icons/ArrowForward'
 import PreviousIcon from '@material-ui/icons/ArrowBack'
+import Launch from '@material-ui/icons/Launch'
 import clsx from 'clsx'
 import React from 'react'
 import Link from 'next/link'
@@ -15,7 +16,13 @@ const useStyles = makeStyles(theme => ({
   link: {
     textTransform: 'none',
     fontWeight: 'normal',
-    margin: theme.spacing(6, 0, 3, 2),
+    margin: theme.spacing(0, 0, 3, 2),
+  },
+  footer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: theme.spacing(6, 0, 0, 0),
   },
 }))
 
@@ -28,16 +35,30 @@ export default function Footer({ guide, navData }) {
   const prevGuide = guideIdx > 0 ? orderedGuides[guideIdx - 1] : null
   const nextGuide =
     guideIdx >= 0 && guideIdx < orderedGuides.length ? orderedGuides[guideIdx + 1] : null
+  const editGuide = `https://github.com/layer0-docs/layer0-docs/edit/master/guides/${guide}.md`
 
   if (!prevGuide && !nextGuide) {
     return null
   }
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      {prevGuide && <GuideLink variant="previous" guide={prevGuide} />}
-      {nextGuide && <GuideLink variant="next" guide={nextGuide} />}
-    </div>
+    <>
+      <Button
+        href={editGuide}
+        target="_blank"
+        variant="text"
+        size="small"
+        startIcon={<Launch style={{ fontSize: '.8rem' }} />}
+        color="secondary"
+        style={{ fontSize: '.7rem' }}
+      >
+        Edit this guide on Github
+      </Button>
+      <div className={classes.footer}>
+        {prevGuide && <GuideLink variant="previous" guide={prevGuide} />}
+        {nextGuide && <GuideLink variant="next" guide={nextGuide} />}
+      </div>
+    </>
   )
 }
 

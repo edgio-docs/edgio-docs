b33b80abe943a27a321ebfb6dec6fc2ed67f85cc
Jeff Patzer <jeff.patzer@layer0.co>
2021-11-09 07:46:44 -0500

    remove this, should commit

--- components/getVersions.js
+++ components/getVersions.js
@@ -6,9 +6,6 @@ export default async function getVersions() {
   // Here we add a cache buster to ensure that when the cache is cleared we get the latest versions file from
   // GitHub
 
-  // TEMP, do not commit
-  return []
-
   const verRes = await fetch(`${DOCS_PAGES_REPO_URL}/versions.csv?nc=${new Date().getTime()}`)
 
   return semverRSort((await verRes.text()).split(',').map(ver => ver.trim()))

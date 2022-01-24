ff94f9f11e662856fef00368b280dcb090467259
adebiyial <adebiyiadedotun9@gmail.com>
2022-01-24 09:47:31 +0100

    Lint file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@codesandbox/sandpack-react/dist/index.css';
 import '@docsearch/css';
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import * as React from 'react';
@@ -11,7 +11,6 @@ import '../styles/index.css';
 import '../styles/nprogress.css';
 import '../styles/sandpack.css';
 
-
 // -> Used for the loader when switching between pages
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -19,7 +18,7 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 const EmptyAppShell: React.FC = ({children}) => <>{children}</>;
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({Component, pageProps}: AppProps) {
   let AppShell = (Component as any).appShell || EmptyAppShell;
   // In order to make sidebar scrolling between pages work as expected
   // we need to access the underlying MDX component.

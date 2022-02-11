9daac2510c6ec2386b2e870f30f9cd27820bb6e4
adebiyi-al <aadedotun@llnw.com>
2022-02-11 20:13:26 +0100

    Style ol and uls

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -23,10 +23,10 @@ const Strong = (strong: JSX.IntrinsicElements['strong']) => (
 );
 
 const OL = (p: JSX.IntrinsicElements['ol']) => (
-  <ol className="ml-6 my-3 list-decimal" {...p} />
+  <ol className="article-ol__list" {...p} />
 );
 const LI = (p: JSX.IntrinsicElements['li']) => (
-  <li className="leading-relaxed mb-1" {...p} />
+  <li className="list-item" {...p} />
 );
 const UL = (p: JSX.IntrinsicElements['ul']) => (
   <ul className="article-ul__list" {...p} />

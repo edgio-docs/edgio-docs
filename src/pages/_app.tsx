2b7dfbe2f8b80ed43d4d8d13546d9184179cc0a2
adebiyi-al <aadedotun@llnw.com>
2022-02-23 16:19:41 +0100

    Remove FB styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
-import '@codesandbox/sandpack-react/dist/index.css';
 import '@docsearch/css';
 import {AppProps} from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import * as React from 'react';
 import '../styles/algolia.css';
-import '../styles/fonts.css';
 import GlobalStyle from '../styles/GlobalStyle';
 import '../styles/nprogress.css';
-import '../styles/sandpack.css';
 
 // -> Used for the loader when switching between pages
 Router.events.on('routeChangeStart', () => NProgress.start());

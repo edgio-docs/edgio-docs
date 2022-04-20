ba7f00732d35624b0d589f50f80fe768230b9efb
Adebiyi Adedotun <aadedotun@llnw.com>
2022-04-20 20:26:03 +0100

    Update CSP rules

--- routes.ts
+++ routes.ts
@@ -65,7 +65,7 @@ const router = new Router()
           `font-src fonts.gstatic.com`,
           `img-src 'self' www.google-analytics.com analytics.twitter.com www.facebook.com px.ads.linkedin.com *.intercomcdn.com tr.lfeeder.com data:`,
           `frame-src www.youtube.com youtu.be player.vimeo.com/*`,
-          `script-src 'unsafe-inline' 'self' 'unsafe-eval' cdn.jsdelivr.net www.googletagmanager.com cdn.segment.com cdn4.mxpnl.com www.google-analytics.com widget.intercom.io sc.lfeeder.com snap.licdn.com connect.facebook.net www.youtube.com js.intercomcdn.com static.hotjar.com s.adroll.com px4.ads.linkedin.com`,
+          `script-src 'unsafe-inline' 'self' 'unsafe-eval' player.vimeo.com/api/player.js cdn.jsdelivr.net www.googletagmanager.com cdn.segment.com cdn4.mxpnl.com www.google-analytics.com widget.intercom.io sc.lfeeder.com snap.licdn.com connect.facebook.net www.youtube.com js.intercomcdn.com static.hotjar.com s.adroll.com px4.ads.linkedin.com`,
           `base-uri 'self'`,
           `frame-ancestors 'self'`,
           `media-src www.youtube.com`,

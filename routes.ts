523b05b69d1635409f35dfc63792583d862d6dd5
Tristan Lee <tristanlee85@gmail.com>
2022-04-21 18:38:42 -0400

    Add CSP

--- routes.ts
+++ routes.ts
@@ -65,7 +65,7 @@ const scriptSrcDomains = [
   'connect.facebook.net',
   'www.youtube.com',
   'js.intercomcdn.com',
-  'static.hotjar.com',
+  '*.hotjar.com',
   's.adroll.com',
   'px4.ads.linkedin.com',
 ].sort();
@@ -102,7 +102,7 @@ const router = new Router()
           `default-src 'self'`,
           `style-src 'unsafe-inline' 'self' fonts.googleapis.com cdn.jsdelivr.net`,
           `font-src fonts.gstatic.com`,
-          `img-src 'self' www.google-analytics.com analytics.twitter.com www.facebook.com px.ads.linkedin.com *.intercomcdn.com tr.lfeeder.com data:`,
+          `img-src 'self' www.google-analytics.com analytics.twitter.com www.facebook.com px.ads.linkedin.com *.intercomcdn.com tr.lfeeder.com data: *.moovweb.net`,
           `frame-src www.youtube.com youtu.be player.vimeo.com`,
           `script-src 'unsafe-inline' 'self' 'unsafe-eval' ${scriptSrcDomains.join(
             ' '

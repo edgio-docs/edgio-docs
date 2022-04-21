517c8820701fca14ce7589569ef866445053126b
Tristan Lee <tristan.lee@moovweb.com>
2022-04-21 18:12:17 -0400

    Clean up prerender requests

--- prerender.ts
+++ prerender.ts
@@ -3,14 +3,17 @@ import {map, flatMap, flatten} from 'lodash';
 import navData from './src/data/SidebarMenuItems';
 
 export default async function prerenderRequests() {
-  const requests = [{path: '/'}];
-
-  return map(
-    flatMap(flatten(navData), (item) => item.routes),
-    (item) => {
-      if (item && item.path.startsWith('/')) {
-        return {path: item.path};
+  return [
+    {path: '/'},
+    ...map(
+      flatMap(flatten(navData), (item) => item.routes),
+      (item) => {
+        if (item && item.path.startsWith('/')) {
+          return {path: item.path};
+        }
       }
-    }
-  );
+    )
+      .sort()
+      .filter(Boolean),
+  ];
 }

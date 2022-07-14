fa2ed230fa782ae657000dd81b8cc06fed313bb1
Adebiyi Adedotun <aadedotun@llnw.com>
2022-07-14 14:53:50 +0100

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@docsearch/css';
 import {Metrics} from '@layer0/rum';
+// @ts-ignore
+import galite from 'ga-lite';
 import {MDXEmbedProvider} from 'mdx-embed';
 import type {AppProps} from 'next/app';
 import Router from 'next/router';
+import {useRouter} from 'next/router';
 import NProgress from 'nprogress';
 import * as React from 'react';
 import {ThemeProvider} from 'styled-components';
@@ -15,6 +18,16 @@ import '../styles/prism.css';
 import '../styles/algolia.css';
 
 // import {VersionProvider} from 'components/versioning';
+
+if (typeof window !== 'undefined') {
+  if (process.env.NODE_ENV === 'production') {
+    galite('create', process.env.NEXT_PUBLIC_GA_TRACKING_ID, 'auto');
+  }
+  const terminationEvent = 'onpagehide' in window ? 'pagehide' : 'unload';
+  window.addEventListener(terminationEvent, function () {
+    galite('send', 'timing', 'JS Dependencies', 'unload');
+  });
+}
 
 new Metrics({token: 'cdc8d6df-476b-4e2d-ae1a-f8c6893a39a8'}).collect();
 
@@ -33,6 +46,19 @@ const theme = {
 const EmptyAppShell: React.FC = ({children}) => <>{children}</>;
 
 export default function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      galite('set', 'page', url);
+      galite('send', 'pageview');
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   let AppShell = (Component as any).appShell || EmptyAppShell;
 
   // In order to make sidebar scrolling between pages work as expected

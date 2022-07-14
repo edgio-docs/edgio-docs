fdaed1a5203307e45a3a580a8ee39efa220e7377
Adebiyi Adedotun <aadedotun@llnw.com>
2022-07-14 16:39:09 +0100

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,22 @@
 import '@docsearch/css';
 import {Metrics} from '@layer0/rum';
-// @ts-ignore
-import galite from 'ga-lite';
 import {MDXEmbedProvider} from 'mdx-embed';
 import type {AppProps} from 'next/app';
 import Router from 'next/router';
-import {useRouter} from 'next/router';
+import Script from 'next/script';
 import NProgress from 'nprogress';
 import * as React from 'react';
-import {ThemeProvider} from 'styled-components';
 
-import '../styles/reset.css';
+import '../styles/algolia.css';
 import '../styles/custom-props.css';
 import '../styles/nprogress.css';
-import '../styles/scrollbar.css';
 import '../styles/prism.css';
-import '../styles/algolia.css';
+import '../styles/reset.css';
+import '../styles/scrollbar.css';
 
-// import {VersionProvider} from 'components/versioning';
+const EmptyAppShell: React.FC = ({children}) => <>{children}</>;
 
-if (typeof window !== 'undefined') {
-  if (process.env.NODE_ENV === 'production') {
-    galite('create', process.env.NEXT_PUBLIC_GA_TRACKING_ID, 'auto');
-  }
-  const terminationEvent = 'onpagehide' in window ? 'pagehide' : 'unload';
-  window.addEventListener(terminationEvent, function () {
-    galite('send', 'timing', 'JS Dependencies', 'unload');
-  });
-}
-
+// CWV for Layer0
 new Metrics({token: 'cdc8d6df-476b-4e2d-ae1a-f8c6893a39a8'}).collect();
 
 // -> Used for the loader when switching between pages
@@ -36,29 +24,27 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-// Breakpoints for media queries
-const theme = {
-  breakpoints: {
-    small: '400px',
-  },
-};
+function GAnalytics() {
+  return (
+    <div>
+      <Script
+        id="google-analytics"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-const EmptyAppShell: React.FC = ({children}) => <>{children}</>;
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+        `}
+      </Script>
+    </div>
+  );
+}
 
 export default function MyApp({Component, pageProps}: AppProps) {
-  const router = useRouter();
-
-  React.useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      galite('set', 'page', url);
-      galite('send', 'pageview');
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   let AppShell = (Component as any).appShell || EmptyAppShell;
 
   // In order to make sidebar scrolling between pages work as expected
@@ -69,13 +55,10 @@ export default function MyApp({Component, pageProps}: AppProps) {
 
   return (
     <AppShell>
-      {/* <VersionProvider> */}
-      <ThemeProvider theme={theme}>
-        <MDXEmbedProvider>
-          <Component {...pageProps} />
-        </MDXEmbedProvider>
-      </ThemeProvider>
-      {/* </VersionProvider> */}
+      <GAnalytics />
+      <MDXEmbedProvider>
+        <Component {...pageProps} />
+      </MDXEmbedProvider>
     </AppShell>
   );
 }

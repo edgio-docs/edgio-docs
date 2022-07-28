3a707dd595c42516da2e073eb9f5e1968e488963
Adebiyi Adedotun <aadedotun@llnw.com>
2022-07-28 09:52:08 +0100

    Remove secondary nav items from side bar menu items
    Yet to remove the package APIs.

--- src/data/SidebarMenuItems.tsx
+++ src/data/SidebarMenuItems.tsx
@@ -687,20 +687,6 @@ const SidebarMenuItems: ISidebarMenuItems = [
       ],
     },
     {
-      title: 'Videos',
-      icon: <IconVideos />,
-      iconDark: <IconVideosDark />,
-      path: 'videos',
-      sortRoutes: true,
-      routes: [
-        {
-          title: `${PRODUCT} Tutorials`,
-          path: 'https://vimeo.com/user/776463/folder/9270726',
-          external: true,
-        },
-      ],
-    },
-    {
       title: 'Package APIs',
       icon: <IconFolder />,
       iconDark: <IconFolderDark />,
@@ -742,38 +728,6 @@ const SidebarMenuItems: ISidebarMenuItems = [
           separator: true,
         },
       ],
-    },
-  ],
-  [
-    {
-      title: 'Fiddle',
-      icon: <IconFiddle />,
-      iconDark: <IconFiddleDark />,
-      path: 'https://fiddle.layer0.co/?sgId=7bc47c45-c1d6-4189-b416-552581d86006',
-    },
-    {
-      title: 'Forum',
-      icon: <IconForum />,
-      iconDark: <IconForumDark />,
-      path: 'https://forum.layer0.co/?sgId=7bc47c45-c1d6-4189-b416-552581d86006',
-    },
-    {
-      title: 'Status',
-      icon: <IconStatus />,
-      iconDark: <IconStatusDark />,
-      path: 'https://status.layer0.co/?sgId=7bc47c45-c1d6-4189-b416-552581d86006',
-    },
-    {
-      title: 'Support',
-      icon: <IconSupport />,
-      iconDark: <IconSupportDark />,
-      path: 'https://app.layer0.co/help?sgId=7bc47c45-c1d6-4189-b416-552581d86006',
-    },
-    {
-      title: 'Edg.io',
-      icon: <IconEdgioSquareLogo />,
-      iconDark: <IconEdgioSquareLogoDark />,
-      path: 'https://edg.io',
     },
   ],
 ];

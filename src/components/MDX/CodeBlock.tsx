2b5a20bfd41373e46a537a36c718f670c541c346
Adebiyi Adedotun <aadedotun@llnw.com>
2022-08-10 20:15:35 +0100

    Refactor codeblocks
    Switched to the prism-react-renderer package

--- src/components/MDX/CodeBlock.tsx
+++ src/components/MDX/CodeBlock.tsx
@@ -1,24 +1,57 @@
 import cn from 'classnames';
-import Prism from 'prismjs';
-import React, {useEffect} from 'react';
-
-import useHydrationIsLoaded from 'utils/hooks/useHydrationIsLoaded';
+import Highlight, {defaultProps, Language} from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/nightOwl';
+import styled from 'styled-components';
 
 export default function CodeBlock({
   language,
   children,
 }: {
-  language: string;
-  children: React.ReactNode;
+  language: Language;
+  children: string;
 }) {
-  const isLoaded = useHydrationIsLoaded();
-  useEffect(() => {
-    Prism.highlightAll();
-  }, [isLoaded]);
-
   return (
-    <pre className={cn('custom-scrollbar', language)}>
-      <code className={`${language}`}>{children}</code>
-    </pre>
+    <Highlight
+      {...defaultProps}
+      theme={undefined}
+      code={children}
+      language={language}>
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={cn('custom-scrollbar', className)} style={style}>
+          {tokens.map((line, i) => (
+            <Line key={i} {...getLineProps({line, key: i})}>
+              <LineNo>{i + 1}</LineNo>
+              <LineContent>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({token, key})} />
+                ))}
+              </LineContent>
+            </Line>
+          ))}
+        </pre>
+      )}
+    </Highlight>
   );
 }
+
+export const Line = styled.div`
+  display: table-row;
+`;
+
+export const LineContent = styled.span`
+  display: table-cell;
+  padding-left: 0.5em;
+`;
+
+export const LineNo = styled.span`
+  display: table-cell;
+  text-align: right;
+  padding-right: 0.5em;
+  user-select: none;
+  letter-spacing: -1px;
+  border-right: 2px solid #363636;
+  position: sticky;
+  left: 0;
+  background: #242424;
+  padding-left: 15px;
+`;

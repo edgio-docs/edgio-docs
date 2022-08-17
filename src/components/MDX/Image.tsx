358537923649fd1b466766a2687dfe3561565b08
Adebiyi Adedotun <aadedotun@llnw.com>
2022-08-17 13:55:53 +0100

    Update the image component to serve from Edgio and render with next/image

--- src/components/MDX/Image.tsx
+++ src/components/MDX/Image.tsx
@@ -13,7 +13,31 @@ const StyledComp = styled.figure`
 `;
 
 export default function Image({src, alt}: {src: string; alt: string}) {
-  const edgeSrc = `https://opt.moovweb.net?quality=30&img=https://docs.layer0.co${src}`;
+  // ??
+  // The idea here is that we can pass a width and height as query params in the image src.
+  // as in: https://example.com/image.png?width=500&height=500
+  // or for relative urls: /image.png?width=500&height=500
+  //
+  // The srcUrl is the URL of the image to be displayed, prepended with a baseUrl.
+  // because relative URLs can't be passed to the URL object and the images are
+  // served with Edgio . see /guides/image_optimization.md
+  //
+  // The dimensions returns an object { "width": "", "height": "" } or {} if no width/height params
+  const isAbsoluteUrl = src.startsWith('https://');
+  const srcUrl = new URL(isAbsoluteUrl ? src : `https://docs.layer0.co${src}`);
+  const dimensions = new URLSearchParams(srcUrl.search);
+  const width = dimensions.get('width');
+  const height = dimensions.get('height');
+
+  const edgeSrc = `https://opt.moovweb.net?img=${srcUrl}`;
+
+  if (width && height) {
+    return (
+      <div className="image">
+        <NextImage {...{src: edgeSrc, alt, width, height}} />
+      </div>
+    );
+  }
 
   return (
     <StyledComp>

ed66afca4ea52840fe8302526e9c9045e050222f
Tristan Lee <tristanlee85@gmail.com>
2022-08-18 12:51:55 -0400

    Format

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -94,7 +94,16 @@ class MyDocument extends Document {
                 })();
 
                 // Clarity tracking code
-                (function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "d9958syk46");
+                (function(c, l, a, r, i, t, y) {
+                  c[a] = c[a] || function() {
+                    (c[a].q = c[a].q || []).push(arguments)
+                  };
+                  t = l.createElement(r);
+                  t.async = 1;
+                  t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+                  y = l.getElementsByTagName(r)[0];
+                  y.parentNode.insertBefore(t, y);
+                })(window, document, "clarity", "script", "d9958syk46");  
               `,
             }}
           />

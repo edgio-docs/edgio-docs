d06029a9ec35f82522419bd1235e00df6f12483b
Tristan Lee <tristanlee85@gmail.com>
2022-08-18 13:50:04 -0400

    Add Clarity tracking tag (#466)
    * Add Clarity tracking tag
    
    * Format
    
    * Update CSP
    
    * Update CSP

--- routes.ts
+++ routes.ts
@@ -56,6 +56,7 @@ const scriptSrcDomains = [
   '*.google-analytics.com',
   '*.googletagmanager.com',
   'googletagmanager.com',
+  '*.clarity.ms',
 ].sort();
 
 const connectSrcDomains = [
@@ -77,6 +78,7 @@ const connectSrcDomains = [
   '*.algolia.net',
   '*.vimeo.com',
   'vimeo.com',
+  '*.clarity.ms',
 ].sort();
 
 const router = new Router()

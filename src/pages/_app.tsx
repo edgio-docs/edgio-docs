c7715d47fdbc5ba619679063a468cb8e31d0e6c6
Rishi Raj Jain <46300090+rishi-raj-jain@users.noreply.github.com>
2022-09-21 13:04:42 +0530

    Add dynamically loaded pages and switch views when navigating for given routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import '@docsearch/css';
 import {Metrics} from '@layer0/rum';
 import {MDXEmbedProvider} from 'mdx-embed';
 import type {AppProps} from 'next/app';
-import Router from 'next/router';
+import dynamic from 'next/dynamic';
+import {useRouter} from 'next/router';
 import Script from 'next/script';
 import NProgress from 'nprogress';
 import * as React from 'react';
+
+// Universal loading page (used in dynamically imported components) which contains the wrapper of each page
+import LoadingFallBackPage from 'components/Fallbacks/Loading';
 
 import '../styles/algolia.css';
 import '../styles/custom-props.css';
@@ -19,10 +23,20 @@ const EmptyAppShell: React.FC = ({children}) => <>{children}</>;
 // CWV for Edgio
 new Metrics({token: 'cdc8d6df-476b-4e2d-ae1a-f8c6893a39a8'}).collect();
 
-// -> Used for the loader when switching between pages
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+// List of fallback components
+const ChangeLogFallBackPage = dynamic(
+  () => import('../components/Fallbacks/Changelog'),
+  {
+    loading: () => <LoadingFallBackPage />,
+  }
+);
+
+// Create a fallback dict
+// Match a route being navigated to
+// and place the fallback/loading versions of those pages
+const fallbackMap = {
+  '/guides/changelog': <ChangeLogFallBackPage />,
+};
 
 function GAnalytics() {
   return (
@@ -44,6 +58,39 @@ function GAnalytics() {
 }
 
 export default function MyApp({Component, pageProps}: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = React.useState(false);
+  const [changingTo, setChangingTo] = React.useState('');
+  React.useEffect(() => {
+    // All of this should execute if JS is available after (if) mounted
+    const handleRouteChange = (url: string, {shallow}: {shallow: any}) => {
+      // Start the spinner
+      NProgress.start();
+      // Set loading to true
+      setLoading(true);
+      // Set the url changing to fallback
+      setChangingTo(url);
+    };
+    const handleRouteComplete = (url: string, {shallow}: {shallow: any}) => {
+      // End the spinner
+      NProgress.done();
+      // Set the url to empty so that always <Component {...pageProps} /> is rendered
+      setChangingTo('');
+      // Set loading to false
+      setLoading(false);
+    };
+    // -> Used for the loader when switching between pages
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteComplete);
+    router.events.on('routeChangeError', () => handleRouteComplete);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteComplete);
+      router.events.on('routeChangeError', () => handleRouteComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   let AppShell = (Component as any).appShell || EmptyAppShell;
 
   // In order to make sidebar scrolling between pages work as expected
@@ -52,7 +99,9 @@ export default function MyApp({Component, pageProps}: AppProps) {
     AppShell = (Component as any)({}).props.originalType.appShell;
   }
 
-  return (
+  return loading && fallbackMap.hasOwnProperty(changingTo) ? (
+    fallbackMap[changingTo]
+  ) : (
     <AppShell>
       <GAnalytics />
       <MDXEmbedProvider>

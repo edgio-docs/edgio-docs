203f07f0f420bb835e199a0b82393d656b7bf3ef
Adebiyi Adedotun <aadedotun@llnw.com>
2022-11-22 10:33:05 +0100

    Revert changes to code line no

--- src/components/MDX/CodeBlock.tsx
+++ src/components/MDX/CodeBlock.tsx
@@ -19,6 +19,7 @@ export default function CodeBlock({
         <pre className={cn('custom-scrollbar', className)} style={style}>
           {tokens.map((line, i) => (
             <Line key={i} {...getLineProps({line, key: i})}>
+              <LineNo>{i + 1}</LineNo>
               <LineContent>
                 {line.map((token, key) => (
                   <span key={key} {...getTokenProps({token, key})} />
@@ -39,4 +40,17 @@ export const Line = styled.div`
 export const LineContent = styled.span`
   display: table-cell;
   padding-left: 0.5em;
+`;
+
+export const LineNo = styled.span`
+  left: 0px;
+  min-width: 35px;
+  position: sticky;
+  user-select: none;
+  text-align: right;
+  display: table-cell;
+  background: #181717;
+  letter-spacing: -1px;
+  padding-right: 0.5em;
+  border-right: 1px solid rgb(54, 54, 54);
 `;

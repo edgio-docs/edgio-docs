104cf1ebb767280e495f981ac3c51f7a68ba953a
Adebiyi Adedotun <aadedotun@edg.io>
2022-12-14 11:13:14 +0100

    Fix page margin for mobile

--- src/components/Docs/index.tsx
+++ src/components/Docs/index.tsx
@@ -9,7 +9,7 @@ import {siteConfig} from 'siteConfig';
 
 const StyledDocs = styled.div`
   max-width: var(--docs-area-width);
-  margin: 0 auto 500px auto;
+  margin: 0 auto 600px auto;
   display: grid;
   grid-template-columns: 75% 1fr;
 

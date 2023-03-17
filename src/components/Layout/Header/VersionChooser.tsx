01e93082512fe53f6d747e484ae7bd862ae355c7
Tristan Lee <tristanlee85@gmail.com>
2023-03-17 17:23:44 -0400

    Style changes to version chooser

--- src/components/Layout/Header/VersionChooser.tsx
+++ src/components/Layout/Header/VersionChooser.tsx
@@ -1,56 +1,66 @@
+import {ExpandMore as ExpandMoreIcon} from '@mui/icons-material';
+import {styled} from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 import Link from 'next/link';
-import {useState} from 'react';
-//@ts-ignore
-import {Button} from 'styled-button-component';
-import styled from 'styled-components';
-//@ts-ignore
-import {Dropdown, DropdownItem, DropdownMenu} from 'styled-dropdown-component';
 
 import {getVersionedConfigs} from 'utils/config';
 import useConditioning from 'utils/hooks/useConditioning';
 
-const StyledDropdown = styled(Dropdown)`
-  background-color: var(--bg-secondary);
-  color: var(--text-primary);
-  margin-left: 15px;
+const linkStyles = {
+  color: 'var(--text-primary)',
+  textDecoration: 'none',
+  '&:visited': {
+    color: 'var(--text-primary)',
+  },
+};
 
-  button {
-    padding: 4px;
-    font-size: 14px;
-    background-color: var(--bg-secondary) !important;
-    color: var(--text-primary) !important;
-  }
-`;
+const VersionItem = styled(MenuItem)(() => ({
+  '&.MuiMenuItem-root.Mui-selected': {
+    backgroundColor: 'var(--select-item-selected-bg)',
+  },
+  '&.MuiMenuItem-root:hover': {
+    backgroundColor: 'var(--select-item-hover-bg)',
+  },
+}));
 
-const StyledDropdownMenu = styled(DropdownMenu)`
-  background-color: var(--bg-secondary);
-  color: var(--text-primary);
-  border-color: var(--colors-blue0) !important;
-`;
+const VersionSelect = styled(Select)(() => ({
+  '&': {
+    marginLeft: 20,
+  },
+  '& .MuiSelect-icon': {
+    color: 'var(--text-primary)',
+  },
+  '& .MuiSelect-select': {
+    color: 'var(--text-primary)',
+    backgroundColor: 'var(--select-bg)',
+    border: '1px solid var(--sidebar-link-primary)',
+    fontSize: 14,
+    padding: '5px 14px',
+    '& a': linkStyles,
+  },
+  '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+    borderColor: 'var(--select-border-color)',
+  },
+  '&:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: 'var(--select-border-color)',
+  },
+}));
 
-const StyledDropdownItem = styled(DropdownItem)`
-  background-color: var(--bg-secondary);
-  color: var(--text-primary);
-  font-size: 14px;
-  ${({selected}) =>
-    selected
-      ? `
-    font-weight: bold;
-    color: var(--colors-blue0);
-  `
-      : `
-        font-weight: normal;
-        `};
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-`;
+const menuProps = {
+  sx: {
+    '& a': linkStyles,
+    '& .MuiMenuItem-root': {
+      fontSize: 14,
+    },
+    '& .MuiMenu-paper': {
+      backgroundColor: 'var(--select-bg)',
+    },
+  },
+};
 
 export default function VersionChooser() {
   const {version} = useConditioning();
-  const [hidden, setHidden] = useState(true);
   const {selectedVersion, latestVersion} = version;
 
   const prefixedLatestVersion = `v${latestVersion}`;
@@ -64,26 +74,31 @@ export default function VersionChooser() {
     }))
     .reverse();
 
-  const {label: selectedLabel} = versions.find(
-    ({version}) => version === prefixedSelectedVersion
-  ) || {label: `Applications ${prefixedLatestVersion}`};
+  const onChange = (e: any) => {
+    console.log(e);
+  };
 
   return (
-    <StyledDropdown>
-      <Button dropdownToggle onClick={() => setHidden(!hidden)}>
-        {selectedLabel}
-      </Button>
-      <StyledDropdownMenu hidden={hidden} toggle={() => setHidden(!hidden)}>
-        {versions.map(({version, href, label}) => {
-          return (
-            <StyledDropdownItem
-              key={version}
-              selected={version === prefixedSelectedVersion}>
-              <Link href={href}>{label}</Link>
-            </StyledDropdownItem>
-          );
-        })}
-      </StyledDropdownMenu>
-    </StyledDropdown>
+    <VersionSelect
+      sx={{m: 1, minWidth: 150}}
+      size="small"
+      IconComponent={ExpandMoreIcon}
+      MenuProps={menuProps}
+      onChange={onChange}
+      value={prefixedSelectedVersion}
+      variant="outlined"
+      renderValue={(value) => versions.find((v) => v.version === value)?.label}>
+      {versions.map(({version, href, label}) => {
+        return (
+          <Link href={href} key={version}>
+            <VersionItem
+              selected={version === prefixedSelectedVersion}
+              value={version}>
+              {label}
+            </VersionItem>
+          </Link>
+        );
+      })}
+    </VersionSelect>
   );
 }

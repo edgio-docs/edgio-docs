2a6ac4f46584fce90aeec3e7f0ac0ad612b46547
Tristan Lee <tristanlee85@gmail.com>
2023-05-04 16:15:13 -0400

    [COMSUP-7] Always include version in url (#740)
    * Always include version in path, including latest version
    
    * Fix build errors
    
    * Revert "Revert "Always include version in path, including latest version (#735)""
    
    This reverts commit 056db287cd2d08776bb5eba5057c4d328eacb40f.
    
    * Prerender only latest guides, falling back to SSR.
    
    * Include markdown files in bundle for SSR
    
    * Remove old webpack bundling of homepage and 404
    
    This now gives the option to give older versions a different home page if desired, but mainly just to improve local dev build speed

--- edgio.config.js
+++ edgio.config.js
@@ -8,4 +8,9 @@ module.exports = {
       hostHeader: DOCS_PAGES_DOMAIN,
     },
   },
+
+  // include the raw markdown files in the build for SSR
+  includeFiles: {
+    'src/guides/**/*': true,
+  },
 };

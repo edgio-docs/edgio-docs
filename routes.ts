83d76b97f284824e3ab7da0374614c410d230c23
Jose Sernaque <joesernaque@gmail.com>
2023-05-15 17:33:45 -0700

    Added file extension to rewrite

--- routes.ts
+++ routes.ts
@@ -143,7 +143,7 @@ const router = new Router()
           `v*`
         );
 
-        redirect(`/docs/${targetVersion}/api/:path*`);
+        redirect(`/docs/${targetVersion}/api/:path*:file`);
       });
     }
   )
@@ -162,7 +162,7 @@ const router = new Router()
         `v*`
       );
 
-      redirect(`/docs/${targetVersion}/api/:path*`);
+      redirect(`/docs/${targetVersion}/api/:path*:file`);
     });
   })
   // match current api docs without terminating /,

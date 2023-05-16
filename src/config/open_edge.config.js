b8acd7f680c11b9607ce98a5b81a21a4a4ac2433
Jose Sernaque <joesernaque@gmail.com>
2023-05-16 09:16:46 -0700

    Open Edge config and nav files

---
+++ src/config/open_edge.config.js
@@ -0,0 +1,11 @@
+import {default as base} from './base.config';
+
+const COMPANY_NAME = 'Edgio';
+const PORTAL_OPEN_EDGE = `${COMPANY_NAME} Control Portal`;
+
+const config = {
+  COMPANY_NAME,
+  PORTAL_OPEN_EDGE,
+};
+
+export default config;

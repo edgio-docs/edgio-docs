35dbd3b4ba9aced01456cb27e3166a60e5dd63d4
Jose Sernaque <joesernaque@gmail.com>
2023-05-16 09:22:16 -0700

    Added const for Open Edge

--- src/config/open_edge.config.js
+++ src/config/open_edge.config.js
@@ -2,10 +2,12 @@ import {default as base} from './base.config';
 
 const COMPANY_NAME = 'Edgio';
 const PORTAL_OPEN_EDGE = `${COMPANY_NAME} Control Portal`;
+const PRODUCT_OPEN_EDGE = 'Open Edge';
 
 const config = {
   COMPANY_NAME,
   PORTAL_OPEN_EDGE,
+  PRODUCT_OPEN_EDGE,
 };
 
 export default config;

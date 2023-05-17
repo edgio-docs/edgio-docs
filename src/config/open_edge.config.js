3538a0a4a223928ecbd7b6b1d3ceef6d98e54e99
jv1564 <96133041+jv1564@users.noreply.github.com>
2023-05-17 10:36:20 -0500

    Update open_edge.config.js
    updated control portal constant

--- src/config/open_edge.config.js
+++ src/config/open_edge.config.js
@@ -1,12 +1,12 @@
 import {default as base} from './base.config';
 
 const COMPANY_NAME = 'Edgio';
-const PORTAL_OPEN_EDGE = `${COMPANY_NAME} Control Portal`;
+const PORTAL_CONTROL = 'Control Portal';
 const PRODUCT_OPEN_EDGE = 'Open Edge';
 
 const config = {
   COMPANY_NAME,
-  PORTAL_OPEN_EDGE,
+  PORTAL_CONTROL,
   PRODUCT_OPEN_EDGE,
 };
 

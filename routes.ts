ba0ec39492b197fbbf374642834a50644c631d9a
Tristan Lee <tristan.lee@moovweb.com>
2023-06-21 16:04:20 -0400

    test

--- routes.ts
+++ routes.ts
@@ -106,77 +106,76 @@ router.match('/docs/versions', {
 // proxy v?.x api docs to the latest version
 [3, 4, 5, 6, 7].forEach((v) => {
   // proxy /docs/v?.x to the latest version
-  router
-    .match(`/docs/v${v}.x/:path*`, ({compute, send}) => {
-      compute(async (req, res) => {
-        // fetch the list of current published versions
-        const versions = await (
-          await fetch('https://docs.edg.io/docs/versions')
-        ).text();
+  router.match(`/docs/v${v}.x/:path*`, ({compute, send}) => {
+    compute(async (req, res) => {
+      // fetch the list of current published versions
+      const versions = await (
+        await fetch('https://docs.edg.io/docs/versions')
+      ).text();
 
-        const targetVersion = semverMaxSatisfying(
-          versions.replace(/\n/g, '').split(','),
-          `v${v}.x`
-        );
+      const targetVersion = semverMaxSatisfying(
+        versions.replace(/\n/g, '').split(','),
+        `v${v}.x`
+      );
 
-        let targetPath = req.path.replace(`/docs/v${v}.x`, `/${targetVersion}`);
-        const lastPathSegment = targetPath.split('/').reverse()[0];
-        const hasTrailingSlash = targetPath.endsWith('/');
-        const hasFileExtension = lastPathSegment.includes('.');
-        const slashSeparator = hasTrailingSlash ? '' : '/';
+      let targetPath = req.path.replace(`/docs/v${v}.x`, `/${targetVersion}`);
+      const lastPathSegment = targetPath.split('/').reverse()[0];
+      const hasTrailingSlash = targetPath.endsWith('/');
+      const hasFileExtension = lastPathSegment.includes('.');
+      const slashSeparator = hasTrailingSlash ? '' : '/';
 
-        // set path to index.html if it doesn't end with a file extension
-        if (!hasFileExtension) {
-          targetPath = targetPath + slashSeparator + 'index.html';
-        }
+      // set path to index.html if it doesn't end with a file extension
+      if (!hasFileExtension) {
+        targetPath = targetPath + slashSeparator + 'index.html';
+      }
 
-        const upstreamRes = await fetch(
-          `https://${DOCS_PAGES_DOMAIN}${targetPath}`
-        );
-        const upstreamResBody = await upstreamRes.text();
-        res.body = upstreamResBody;
+      const upstreamRes = await fetch(
+        `https://${DOCS_PAGES_DOMAIN}${targetPath}`
+      );
+      const upstreamResBody = await upstreamRes.text();
+      res.body = upstreamResBody;
 
-        // due to relative paths in the response, if the path doesn't end with a trailing
-        // slash (eg. /api/core), then assets will be requested from the wrong path (eg. /api/assets/...)
-        // so we need to rewrite the paths to include the last path segment
-        if (!hasTrailingSlash && !hasFileExtension) {
-          const $ = load(upstreamResBody ?? '');
+      // due to relative paths in the response, if the path doesn't end with a trailing
+      // slash (eg. /api/core), then assets will be requested from the wrong path (eg. /api/assets/...)
+      // so we need to rewrite the paths to include the last path segment
+      if (!hasTrailingSlash && !hasFileExtension) {
+        const $ = load(upstreamResBody ?? '');
 
-          // prepend ${lastPathSegment} to all elements with an href or src attribute
-          $('*[href], *[src]').each((i, el) => {
-            const $el = $(el);
-            const href = $el.attr('href');
-            const src = $el.attr('src');
+        // prepend ${lastPathSegment} to all elements with an href or src attribute
+        $('*[href], *[src]').each((i, el) => {
+          const $el = $(el);
+          const href = $el.attr('href');
+          const src = $el.attr('src');
 
-            if (href) {
-              $el.attr('href', `${lastPathSegment}/${href}`);
-            }
+          if (href) {
+            $el.attr('href', `${lastPathSegment}/${href}`);
+          }
 
-            if (src) {
-              $el.attr('src', `${lastPathSegment}/${src}`);
-            }
-          });
+          if (src) {
+            $el.attr('src', `${lastPathSegment}/${src}`);
+          }
+        });
 
-          res.body = $.html();
-        }
-      });
-    })
-
-    // proxy api docs assets
-    .match('/docs/:version/api/:path*:file(\\.[css|js|html|json|png]+)', {
-      origin: {
-        set_origin: 'api',
-      },
-      url: {
-        url_rewrite: [
-          {
-            source: '/docs/:version/api/:path*:file',
-            destination: '/:version/api/:path*:file',
-            syntax: 'path-to-regexp',
-          },
-        ],
-      },
+        res.body = $.html();
+      }
     });
+  });
+
+  // proxy api docs assets
+  // .match('/docs/:version/api/:path*:file(\\.[css|js|html|json|png]+)', {
+  //   origin: {
+  //     set_origin: 'api',
+  //   },
+  //   url: {
+  //     url_rewrite: [
+  //       {
+  //         source: '/docs/:version/api/:path*:file',
+  //         destination: '/:version/api/:path*:file',
+  //         syntax: 'path-to-regexp',
+  //       },
+  //     ],
+  //   },
+  // });
 });
 
 // redirects

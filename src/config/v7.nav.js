299010ef794e2f1b448bf294fc6f9a71ebb8e954
Jose Sernaque <joesernaque@gmail.com>
2023-08-04 11:57:39 -0700

    Added RTLD Bot manager log fields to nav

--- src/config/v7.nav.js
+++ src/config/v7.nav.js
@@ -563,12 +563,16 @@ const nav = {
               path: 'logs/rtld/log_fields_rtld_cdn',
             },
             {
+              title: 'Log Fields (RTLD WAF)',
+              path: 'logs/rtld/log_fields_rtld_waf',
+            },
+            {
               title: 'Log Fields (RTLD Rate Limiting',
               path: 'logs/rtld/log_fields_rtld_rate_limiting',
             },
             {
-              title: 'Log Fields (RTLD WAF)',
-              path: 'logs/rtld/log_fields_rtld_waf',
+              title: 'Log Fields (RTLD Bot)',
+              path: 'logs/rtld/log_fields_rtld_bot_manager',
             },
           ],
         },

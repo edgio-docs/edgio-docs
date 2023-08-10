5720978ee094f4eca1d4e55430cb2cd92d3bcde2
Tristan Lee <tristanlee85@gmail.com>
2023-08-10 10:53:59 -0400

    Generate sitemap on deploy

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,16 @@ module.exports = {
   priority: 0.7,
   sitemapSize: 5000,
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: '/docs/v*',
+      },
+    ],
+  },
 };

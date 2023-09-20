9a090d98dff6abfaa09cd54bf8120121d60f3d53
Bartholomaeus Gillner <bgillner@llnw.com>
2023-09-20 10:25:59 +0200

    Pretty print Swagger JSON for better diffing

--- rest_api/scripts/pull_console_swagger.sh
+++ rest_api/scripts/pull_console_swagger.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# Local dev
+# curl http://localhost:3000/api/swagger_doc | jq > tmp/console_swagger.json
 # Dev / Sandbox
-# curl https://api.edgio-dev.app/api/swagger_doc -o tmp/console_swagger.json
+# curl https://api.edgio-dev.app/api/swagger_doc | jq > tmp/console_swagger.json
 # Stage
-# curl https://api.edgio-stage.app/api/swagger_doc -o tmp/console_swagger.json
+# curl https://api.edgio-stage.app/api/swagger_doc | jq > tmp/console_swagger.json
 
 # Production
-curl https://api.edgio.app/api/swagger_doc -o tmp/console_swagger.json
+curl https://api.edgio.app/api/swagger_doc | jq > tmp/console_swagger.json

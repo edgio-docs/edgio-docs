78f3dbe6969032f80648e6fecd18405abb179644
Tristan Lee <tristanlee85@gmail.com>
2024-01-10 21:51:34 -0500

    WIP

---
+++ src/config/uplynk.config.js
@@ -0,0 +1,9 @@
+import {default as base} from './base.config';
+
+const PRODUCT = 'Uplynk';
+
+const config = {
+  PRODUCT_,
+};
+
+export default config;
---
+++ src/config/uplynk.nav.js
@@ -0,0 +1,13 @@
+const nav = {
+  title: null,
+  path: '/uplynk',
+  routes: [
+    {
+      title: 'Uplynk',
+      path: 'home',
+      icon: 'spark',
+    },
+  ],
+};
+
+export default nav;

9d4c8cfb077a3f0b67c08f4b1161d8e2a5617cf6
Tristan Lee <tristanlee85@gmail.com>
2024-04-03 18:10:05 -0400

    Remove OE link

--- src/config/home/sections.ts
+++ src/config/home/sections.ts
@@ -332,10 +332,6 @@ export const sections: HomepageSectionGroup[] = [
             title: 'FAQs',
             path: 'https://support.limelight.com/public/en/Content/Open_Edge/Open_Edge_FAQs.htm',
           },
-          {
-            title: 'rDNS Requirements',
-            path: 'https://support.limelight.com/public/en/Content/Resources/PDF/Open_Edge_rDNS_2024.pdf',
-          },
         ],
       },
     ],

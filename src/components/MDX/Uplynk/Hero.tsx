78083c1c7e58801932d7a8664900c49c003c6392
Tristan Lee <tristanlee85@gmail.com>
2024-05-26 17:19:51 -0400

    Next 13 - image codemod

--- src/components/MDX/Uplynk/Hero.tsx
+++ src/components/MDX/Uplynk/Hero.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
+import Image from 'next/legacy/image';
 import styled from 'styled-components';
 
 import Container, {StyledContainer} from '../../Layout/Container';

35b5c6070494db82bbe690d3dda3ca7b2eef1cc3
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2024-08-15 13:40:59 -0400

    Add Overview page to Edge Functions nav

--- src/config/applications/v7.nav.js
+++ src/config/applications/v7.nav.js
@@ -255,6 +255,10 @@ const nav = {
       icon: 'edge-functions',
       routes: [
         {
+          title: 'Overview',
+          path: 'edge_functions'
+        },
+        {
           title: 'Caching',
           path: 'edge_functions/caching',
         },

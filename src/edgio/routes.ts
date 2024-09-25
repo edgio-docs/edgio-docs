2b55208316dc56cf5b273216bc137661b0fcea77
Jose Sernaque <joesernaque@gmail.com>
2024-09-25 16:46:09 -0700

    Added redirects for Partner REST API

--- src/edgio/routes.ts
+++ src/edgio/routes.ts
@@ -221,6 +221,16 @@ router
   })
   .match(/^\/rest_api$/, ({redirect}) => redirect('/rest_api/'));
 
+// -- Partner REST API --
+router
+  .match('/partner_rest_api/:path*', ({serveStatic}) => {
+    serveStatic('partner_rest_api/dist/:path*');
+  })
+  .match(/^\/partner_rest_api\/$/, ({serveStatic}) => {
+    serveStatic('partner_rest_api/dist/rest_api.html');
+  })
+  .match(/^\/partner_rest_api$/, ({redirect}) => redirect('/partner_rest_api/'));
+
 // redirects
 redirects.forEach(([from, to, statusCode]) => {
   router.match(from, ({redirect}) =>
